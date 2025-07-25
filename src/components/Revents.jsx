916a574b40a845ba09baad68a3e8acaa672f88c9
Tanmay Sugandhi <139053193+TanmaySugandhi@users.noreply.github.com>
2025-07-25 18:52:42 +0530

    Added Gallery and more things to homepage

--- src/components/Revents.jsx
+++ src/components/Revents.jsx
@@ -1,40 +1,83 @@
 import React from "react";
-import "./Revents.css"
+import "./Revents.css";
 function Revents() {
   return (
     <>
-    <section class="events-section">
-  <h2>Recent Events & Workshops</h2>
-  <div class="events-grid">
-    <div class="event-card">
-      <img src="/images/rv24.jpg" alt="Event 1" />
-      <div class="event-info">
-        <h3>ROBOVEDA 2024</h3>
-        <p>Our flagship RoboWars event held during Roboveda 2024 saw over 30 teams battle it out in intense one-on-one bot wars.</p>
-        <span class="event-date">March 10, 2024</span>
+      <div className="info-cards">
+        <div className="card">
+          <a className="yellow" href=""><h2>TAB</h2></a>
+          <p>Learn about our technical domains</p>
+        </div>
+        <div className="card highlight">
+          <a className="high" href=""><h2>Projects</h2></a>
+          <p>View our Projects</p>
+        </div>
+        <div className="card">
+          <a className="yellow" href=""><h2>Team</h2></a>
+          <p>Know about our Team</p>
+        </div>
       </div>
-    </div>
-    <div class="event-card">
-      <img src="/images/workshop1.jpg" alt="Workshop 1" />
-      <div class="event-info">
-        <h3>Drone Workshop</h3>
-        <p>Hands-on drone building and flight workshop, introducing members to UAV tech and applications in real world.</p>
-        <span class="event-date">December 2, 2022</span>
-      </div>
-    </div>
-    <div class="event-card">
-      <img src="/images/workshop2.jpg" alt="Workshop 2" />
-      <div class="event-info">
-        <h3>Drone Workshop</h3>
-        <p>Hands-on drone building and flight workshop, introducing members to UAV tech and applications in real world.</p>
-        <span class="event-date">December 2, 2022</span>
-      </div>
-    </div>
-  </div>
-  </section>
+      <section class="events-section">
+        <h1>Gallery</h1>
+        <h2>Recent Events</h2>
+        <div class="events-grid">
+          <div class="event-card">
+            <img src="/assets/workshop2.jpg" alt="Workshop 2" />
+            <div class="event-info">
+              <p>Sreenidhi School</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/rv24.jpg" alt="Event 1" />
+            <div class="event-info">
+              <p>ROBOVEDA 2024</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/workshop1.jpg" alt="Workshop 1" />
+            <div class="event-info">
+              <p>Drone Workshop</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/workshop2.jpg" alt="Workshop 2" />
+            <div class="event-info">
+              <p>IOT Workshop</p>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="events-section">
+        <h2>Recent Achivements</h2>
+        <div class="events-grid">
+          <div class="event-card">
+            <img src="/assets/workshop2.jpg" alt="Workshop 2" />
+            <div class="event-info">
+              <p>Sreenidhi School</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/rv24.jpg" alt="Event 1" />
+            <div class="event-info">
+              <p>ROBOVEDA 2024</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/workshop1.jpg" alt="Workshop 1" />
+            <div class="event-info">
+              <p>Drone Workshop</p>
+            </div>
+          </div>
+          <div class="event-card">
+            <img src="/assets/workshop2.jpg" alt="Workshop 2" />
+            <div class="event-info">
+              <p>IOT Workshop</p>
+            </div>
+          </div>
+        </div>
+      </section>
     </>
-  )
+  );
 }
 
-export default Revents
-
+export default Revents;
